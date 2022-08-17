fbef1343a271e65aaa6e528df8ddafa4c65f6653
Nirmal2349 <nirmalrajr35@gmail.com>
2022-08-17 10:50:50 +0530

    cart-added

--- frontendbag/src/screens/HomeScreen.js
+++ frontendbag/src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import logger from "use-reducer-logger";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Product from "../components/product";
+import { Helmet } from "react-helmet-async";
 // import data from '../data';
 
 const reducer = (state, action) => {
@@ -40,6 +41,9 @@ export default function HomeScreen() {
   }, []);
   return (
     <div>
+      <Helmet>
+        <title>Bags👜</title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
