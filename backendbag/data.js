499734423fe63a36483a36aed30c69acdfd81a76
Nirmal2349 <nirmalrajr35@gmail.com>
2022-08-17 22:57:56 +0530

    jwt-error

--- backendbag/data.js
+++ backendbag/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "Nirmal",
+      email: "nirmalrajr35@gmail.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "Raj",
+      email: "user@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: "1",
