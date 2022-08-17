5084621c334f810d937bbc966350d0a34eead370
Nirmal2349 <nirmalrajr35@gmail.com>
2022-08-17 12:35:49 +0530

    cart-finish

--- backendbag/data.js
+++ backendbag/data.js
@@ -1,6 +1,7 @@
 const data = {
   products: [
     {
+      _id: "1",
       name: "Hand bag",
       slug: "handbag",
       category: "handbag",
@@ -13,6 +14,7 @@ const data = {
       description: "high quality handbag",
     },
     {
+      _id: "2",
       name: "Duffle bag",
       slug: "Duffle bag",
       category: "dufflebag",
@@ -25,6 +27,7 @@ const data = {
       description: "high quality dufflebag",
     },
     {
+      _id: "3",
       name: "Back bag",
       slug: "Back bag",
       category: "backbag",
@@ -37,6 +40,7 @@ const data = {
       description: "high quality backbag",
     },
     {
+      _id: "4",
       name: "Trekking bag",
       slug: "Trekking bag",
       category: "trekkingbag",
